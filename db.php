6a353f6c734c651836e1adcd2512091f76c571af
cs-gg27 <140365802+cs-gg27@users.noreply.github.com>
2024-12-02 13:37:41 -0500

    db.php

--- db.php
+++ db.php
@@ -1,17 +1,36 @@
 <?php
 class Conecta {
-    private $servername = "localhost";
-    private $username = "root";
-    private $password = "";
-    private $dbname = "semestral_dweb";  // Cambié el nombre de la base de datos
+    private $servername = "172.0.158.101";
+    private $username = "cleidys";
+    private $password = "Sydnelg27";
+    private $dbname = "semestral_dweb";  
 
     private $cnn;
 
     public function conectarDB() {
-        $this->cnn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+        // Inicializar la conexión
+        $this->cnn = mysqli_init();
+        
+        // Configurar SSL: asegúrate de tener los archivos de certificado correctos
+        mysqli_ssl_set($this->cnn, NULL, NULL, '/path/to/ca-cert.pem', NULL, NULL);
+
+        // Conectar con SSL
+        mysqli_real_connect(
+            $this->cnn,
+            $this->servername,
+            $this->username,
+            $this->password,
+            $this->dbname,
+            3306, 
+            NULL,
+            MYSQLI_CLIENT_SSL
+        );
+
+        // Verificar si hubo un error en la conexión
         if ($this->cnn->connect_error) {
             die("Conexión fallida: " . $this->cnn->connect_error);
         }
+
         return $this->cnn;
     }
 
